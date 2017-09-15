4e8498c3b26db329ffe69e9e2ce76f4fc1012989
drayton80 <drayton80@DESKTOP-DJ1SP4N>
2017-09-14 21:13:30 -0300

    Att 0.4 - Exercicio 1 Completo

--- ExerciciosEstruturasDeDados/src/SPOJ/Exercicio1.java
+++ ExerciciosEstruturasDeDados/src/SPOJ/Exercicio1.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package SPOJ;
 
 import java.util.Scanner;
-import listas.ListaSeq;
+import listas.*;
 
 /**
  *
@@ -14,7 +9,90 @@ import listas.ListaSeq;
  */
 public class Exercicio1 {
     public static void main(String[] arguments){
-        ListaSeq valores = new ListaSeq();
         Scanner entrada = new Scanner(System.in);
+        int[] valores_s, valores_q;
+        int sn, qm, contador = 0;
+        int[] valores_diferentes;
+        boolean possui_igual;
+        String exibicao_dos_diferentes = "";
+        
+        sn = Integer.parseInt(entrada.nextLine());
+        valores_s = new int[sn];
+        String valores_s_com_espacos = entrada.nextLine();
+        String[] valores_s_sem_espacos = valores_s_com_espacos.split(" ");
+        
+        for(int i = 0; i < valores_s_sem_espacos.length; i++){
+            valores_s[i] = Integer.parseInt(valores_s_sem_espacos[i]);
+        }
+        
+        qm = Integer.parseInt(entrada.nextLine());
+        valores_q = new int[qm];
+        String valores_q_com_espacos = entrada.nextLine();
+        String[] valores_q_sem_espacos = valores_q_com_espacos.split(" ");
+        
+        for(int i = 0; i < valores_q_sem_espacos.length; i++){
+            valores_q[i] = Integer.parseInt(valores_q_sem_espacos[i]);
+        }
+        
+        /* O máximo que o tamanho do array de valores diferentes pode ter é o tamanho
+           da soma dos dois arrays comparados, pois, nesse caso, todos os números de
+           ambos os arrays seriam diferentes. */
+        valores_diferentes = new int[(sn+qm)];
+        
+        for(int i = 0; i < valores_s.length; i++){
+            possui_igual = false;
+            
+            for(int j = 0; j < valores_q.length; j++){
+                if(valores_s[i] == valores_q[j]){
+                    possui_igual = true;
+                }
+            }
+            
+            if(possui_igual != true){
+                valores_diferentes[contador] = valores_s[i];
+                
+                if(contador == 0){
+                    exibicao_dos_diferentes += String.valueOf(valores_s[i]);
+                }else{
+                    exibicao_dos_diferentes += " " + String.valueOf(valores_s[i]);
+                }
+                
+                contador++;
+            }
+        }
+        
+        for(int i = 0; i < valores_q.length; i++){
+            possui_igual = false;
+            
+            for(int j = 0; j < valores_s.length; j++){
+                if(valores_q[i] == valores_s[j]){
+                    possui_igual = true;
+                }
+            }
+            
+            if(possui_igual != true){
+                valores_diferentes[contador] = valores_q[i];
+                
+                if(contador == 0){
+                    exibicao_dos_diferentes += String.valueOf(valores_q[i]);
+                }else{
+                    exibicao_dos_diferentes += " " + String.valueOf(valores_q[i]);
+                }
+                
+                contador++;
+            }
+        }
+        
+        System.out.println(exibicao_dos_diferentes);
     }
+    
+    //Compara dois inteiros e retorna o maior, se os dois forem iguais, retorna
+    //qualquer um dos dois.
+    //private static int compara(int p, int q){
+    //    if(p > q){
+    //        return p;
+    //    }else{
+    //        return q;
+    //    }
+    //}
 }
